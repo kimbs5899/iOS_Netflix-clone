657eab36a724ac93df9aa00d8e9a4bd8601800fc
kimbs5899 <krsder@naver.com>
2023-07-18 10:36:34 +0900

    Initial Commit

---
+++ Netflix_Clone_ex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Clone_ex
+//
+//  Created by Designer on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
